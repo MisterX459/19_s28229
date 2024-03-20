f6631c8ca0869f9c6acef7173a64a060e1138b7f
MisterX459 <s28229@pjwstk.edu.pl>
2024-03-20 20:46:26 +0100

    square_generator was created as separate class

---
+++ pythonProject/square_generator.py
@@ -0,0 +1,16 @@
+import math
+
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+
+        if end < start:
+            raise ValueError("End of the range cannot be less than the start.")
+
+        squares = [x ** 2 for x in range(start, end + 1)]
+        return squares
+
+    def calculate_square_roots(self, numbers):
+
+        square_roots = [math.sqrt(num) for num in numbers]
+        return square_roots

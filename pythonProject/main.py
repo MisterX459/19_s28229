21cc3e8d5223bcba1c1d5958ebd190156c667714
MisterX459 <s28229@pjwstk.edu.pl>
2024-03-20 20:41:31 +0100

    Task #4 was finished

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,3 +1,5 @@
+import math
+
 #Task 1
 square = [x**2 for x in range(1, 11)]
 print("List of squares of numbers:")
@@ -28,4 +30,27 @@ squares_list = square_generator.generate_squares(start, end)
 
 print(f"List of squares of numbers from {start} to {end}:")
 print(squares_list)
+
+#task 4
+
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        square3 = [x**2 for x in range(start, end+1)]
+        return square3
+
+    def calculate_square_roots(self, numbers):
+
+        square_roots = [math.sqrt(num) for num in numbers]
+        return square_roots
+
+
+square_gen = SquareGenerator()
+start = 1
+end = 10
+squares_list = square_gen.generate_squares(start, end)
+square_roots_list = square_gen.calculate_square_roots(squares_list)
+
+print("List of square roots of numbers from {start} to {end}:")
+print(square_roots_list)
 

c9a5433d46d7661ca3a79c068eec829442e82180
MisterX459 <s28229@pjwstk.edu.pl>
2024-03-20 21:06:38 +0100

    task #10 was done

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -122,3 +122,43 @@ except ValueError as e:
 cubes_list = cubic_gen.generate_cubes(start, end)
 print(f"List of cubes of numbers from {start} to {end}:")
 print(cubes_list)
+
+#task 10
+
+from abc import ABC, abstractmethod
+class SquareGenerator(ABC):
+    @abstractmethod
+    def generate_squares(self, start, end):
+        pass
+    def calculate_square_roots(self, numbers):
+        square_roots = [math.sqrt(num) for num in numbers]
+        return square_roots
+class CubicGenerator(SquareGenerator):
+    def generate_squares(self, start, end):
+
+        if start >= end:
+            raise ValueError("Start of the range must be less than the end.")
+
+        squares = [x ** 2 for x in range(start, end + 1)]
+        return squares
+
+    def generate_cubes(self, start, end):
+        cubes = [x ** 3 for x in range(start, end + 1)]
+        return cubes
+
+
+
+cubic_gen = CubicGenerator()
+start = 1
+end = 10
+
+try:
+    squares_list = cubic_gen.generate_squares(start, end)
+    print("List of squares of numbers from {start} to {end}:")
+    print(squares_list)
+except ValueError as e:
+    print("Error:", e)
+
+cubes_list = cubic_gen.generate_cubes(start, end)
+print("List of cubes of numbers from {start} to {end}:")
+print(cubes_list)

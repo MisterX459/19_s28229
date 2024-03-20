924fffc6e7b4d1c71d2eaf394c6c88c2adbec0e1
MisterX459 <s28229@pjwstk.edu.pl>
2024-03-20 20:36:55 +0100

    Task #3 was finished

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -14,3 +14,18 @@ square2_list = generate_square2(start, end)
 
 print(f"List of squares of numbers from {start} to {end}:")
 print(square2_list)
+
+#task 3
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        square3 = [x**2 for x in range(start, end+1)]
+        return square3
+square_generator = SquareGenerator()
+start = 1
+end = 10
+squares_list = square_generator.generate_squares(start, end)
+
+print(f"List of squares of numbers from {start} to {end}:")
+print(squares_list)
+
